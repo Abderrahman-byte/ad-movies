191e9f17e2d4f20b18aac71906f4cb509655f3c8
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-07-24 13:00:13 +0100

    Add Favicon

--- webpack.prod.js
+++ webpack.prod.js
@@ -18,6 +18,7 @@ module.exports = merge(common, {
     optimization: {
         minimizer: [new OptimizeCssAssetsPlugin(), new TerserPlugin(), new HtmlWebpackPlugin({
             template: "./src/index.html",
+            favicon: "./src/assets/favicon.ico",
             minify: {
                 removeAttributeQuotes: true,
                 removeComments: true,
