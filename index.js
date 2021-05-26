0c4f7dd1209c07eef7f107d9f5561e8c48789584
Abderrahman-byte <el.abderrahman00@gmail.com>
2021-05-26 13:22:50 +0100

    Run on 0.0.0.0:80 if user is root

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const path = require('path')
+const os = require('os')
 const express = require('express')
+
+const PORT = os.userInfo().uid === 0 ? 80 : 8080
+const ADDR = os.userInfo().uid === 0 ? '0.0.0.0' : '127.0.0.1'
 const App = express()
 
 App.use('/static/', express.static(path.join(__dirname, 'build/static'), {
@@ -10,4 +14,6 @@ App.get('/', (request, response) => {
     response.sendFile(path.join(__dirname, 'build/index.html'))
 })
 
-App.listen(8080)
+App.listen(PORT, ADDR, (err) => {
+    console.log(`[*] Listining on ${ADDR}:${PORT}`)
+})
